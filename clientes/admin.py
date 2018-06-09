d44456cd3ea211289a1c8d1b72cbb4b9840f6a9c
Lucas Polo <pradopolo@Wgmail.com>
2018-06-09 13:21:47 -0300

    Customização dos campos de clientes dentro do Admin
    close #16

--- clientes/admin.py
+++ clientes/admin.py
@@ -3,8 +3,22 @@ from .models import Person, Documento, Venda, Produto
 
 
 class PersonAdmin(admin.ModelAdmin):
-    fields = ['first_name', 'last_name', 'age', 'salary', 'bio', 'photo', 'doc']
-    list_display = fields
+    fieldsets = (
+        (
+            'Dados pessoais',
+            {
+                'fields': ('doc', 'first_name', 'last_name',)
+            }
+        ),
+        (
+            'Dados complementares',
+            {
+                'fields': ('age', 'salary', 'bio', 'photo',)
+            }
+        )
+    )
+    # fields = ['doc', ('first_name', 'last_name'), ('age', 'salary'), 'bio', 'photo']
+    list_display = ['nome_completo', 'age', 'salary', 'bio', 'photo', 'doc']
 
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Documento)

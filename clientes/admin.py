feb7ad68f48fd23d4f170f085964f5aa53be77ff
Lucas Polo <pradopolo@Wgmail.com>
2018-06-09 12:48:45 -0300

    Personalização do Django Admin - Fields do Cliente
    close #14

--- clientes/admin.py
+++ clientes/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from .models import Person, Documento, Venda, Produto
 
 
-admin.site.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    fields = ['first_name', 'last_name', 'age', 'salary', 'bio', 'photo', 'doc']
+    list_display = fields
+
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Documento)
 admin.site.register(Venda)
 admin.site.register(Produto)

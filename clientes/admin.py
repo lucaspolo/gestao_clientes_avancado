22dde2fcfd32d040ee846c2300ed8e1e1fa0d19f
Lucas Polo <pradopolo@Wgmail.com>
2018-06-10 20:40:06 -0300

    Admin: alterando modo de exibição da coluna foto
    Ao invés de exibir a URL, será exibido se possui a foto ou não.
    close #20

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 from .models import Person, Documento, Venda, Produto
 
 
@@ -18,7 +19,45 @@ class PersonAdmin(admin.ModelAdmin):
         )
     )
     # fields = ['doc', ('first_name', 'last_name'), ('age', 'salary'), 'bio', 'photo']
-    list_display = ['nome_completo', 'age', 'salary', 'bio', 'photo', 'doc']
+
+    list_display = ['nome_completo', 'age', 'salary', 'bio', 'tem_foto', 'doc'] #Descreve quais dados são exibidos
+
+    def tem_foto(self, obj: Person) -> bool:
+        if obj.photo:
+            return 'Sim'
+        else:
+            return 'Não'
+
+    tem_foto.short_description = "Possui foto?"
+
+    class SalaryListFilter(admin.SimpleListFilter):
+        """
+        Esta classe permite que seja criado filtros para o Django Admin
+        """
+        title = _('Faixa salarial') # Titulo exibido
+        parameter_name =  'salary' # Parametro que se baseia o filtro
+
+        def lookups(self, request, model_admin):
+            """
+            Define quais os nomes das faixas e suas descrições (estas exibidas em tela)
+            """
+            return (
+                ('<10000', _('Menor que 10 mil')),
+                ('>=10000', _('Maior ou igual a 10 mil')),
+            )
+
+        def queryset(self, request, queryset):
+            """
+            Método que efetivamente filtra, chamado uma vez para cada faixa
+            """
+            if self.value() == '<10000':
+                return queryset.filter(salary__lt = 10000)
+
+            if self.value() == '>=10000':
+                return queryset.filter(salary__gte=10000)
+
+    list_filter = (SalaryListFilter, )
+
 
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Documento)

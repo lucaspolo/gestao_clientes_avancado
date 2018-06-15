7582eb0800fb40862251a6b591805a0d0f5dd705
Lucas Polo <pradopolo@Wgmail.com>
2018-06-15 13:51:39 -0300

    Admin: alterado mensagens de cabeçalho, linguagem e timezone
    close #28

--- gestao_clientes/urls.py
+++ gestao_clientes/urls.py
@@ -35,3 +35,7 @@ if settings.DEBUG:
     urlpatterns = [
         path("__debug__/", include(debug_toolbar.urls))
     ] + urlpatterns
+
+admin.site.site_header = "Gestão de Clientes"
+admin.site.index_title = "Administração"
+admin.site.site_title = "Seja bem vindo ao Gestão de Clientes"

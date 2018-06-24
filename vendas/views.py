fc991a01922aa2f14c1ffbeec2b65e1083db3d0b
Lucas Polo <pradopolo@Wgmail.com>
2018-06-24 20:57:42 -0300

    Inserida mais métricas no dashboard de vendas
    close #52

--- vendas/views.py
+++ vendas/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Avg
+from django.db.models import Avg, Min, Max, Count
 from django.shortcuts import render
 
 from django.views import View
@@ -9,7 +9,18 @@ from vendas.models import Venda
 class DashboardView(View):
     def get(self, request):
         media = Venda.objects.all().aggregate(Avg('valor'))['valor__avg']
-        valores = {
+        media_desc = Venda.objects.all().aggregate(Avg('desconto'))['desconto__avg']
+        min = Venda.objects.all().aggregate(Min('valor'))['valor__min']
+        max = Venda.objects.all().aggregate(Max('valor'))['valor__max']
+        numero_de_pedidos = Venda.objects.all().aggregate(Count('id'))['id__count']
+        numero_de_pedidos_nfe_emitida = Venda.objects.filter(nfe_emitida=True).aggregate(Count('id'))['id__count']
+
+        data = {
             'media': media,
+            'media_desc': media_desc,
+            'min': min,
+            'max': max,
+            'numero_de_pedidos': numero_de_pedidos,
+            'numero_de_pedidos_nfe_emitida': numero_de_pedidos_nfe_emitida,
         }
-        return render(request, 'vendas/dashboard.html', valores)
+        return render(request, 'vendas/dashboard.html', data)

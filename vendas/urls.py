ccffa235284d3e4eb0fdf9f6a86f0839deddae0e
Lucas Polo <pradopolo@Wgmail.com>
2018-06-24 20:35:48 -0300

    Criada página de dashboard com dados de venda
    close #50

--- vendas/urls.py
+++ vendas/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
 
+from vendas.views import DashboardView
+
 urlpatterns = [
-    # path('', RedirectView.as_view(url=reverse_lazy('person_list')), name="root"),
-    # path('list/', PersonList.as_view(), name="person_list"),
-    # path('rich_list/', RichPeopleList.as_view(), name="rich_list"),
-    # path('detail/<int:pk>', PersonDetail.as_view(), name="person_detail"),
-    # path('new/', PersonCreate.as_view(), name="person_new"),
-    # path('update/<int:pk>/', PersonUpdate.as_view(), name="persons_update"),
-    # path('delete/<int:pk>/', PersonDelete.as_view(), name="persons_delete"),
-    # path('produto_bulk/', ProdutoBulk.as_view(), name="produto_delete"),
+    path('dashboard', DashboardView.as_view(), name="dashboard"),
 ]
--- vendas/views.py
+++ vendas/views.py
@@ -1,3 +1,15 @@
+from django.db.models import Avg
 from django.shortcuts import render
 
-# Create your views here.
+from django.views import View
+
+from vendas.models import Venda
+
+
+class DashboardView(View):
+    def get(self, request):
+        media = Venda.objects.all().aggregate(Avg('valor'))['valor__avg']
+        valores = {
+            'media': media,
+        }
+        return render(request, 'vendas/dashboard.html', valores)

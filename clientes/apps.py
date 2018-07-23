c5bee856456065a7dfe1bbae989e8b3bdd0f1cde
Lucas Polo <pradopolo@Wgmail.com>
2018-07-23 13:20:59 -0300

    Enviando e-mail para admins da aplicação ao iniciar
    close #84

--- clientes/apps.py
+++ clientes/apps.py
@@ -1,5 +1,17 @@
+from decouple import config
 from django.apps import AppConfig
+from django.core.mail import mail_admins
 
 
 class ClientesConfig(AppConfig):
     name = 'clientes'
+
+    def ready(self):
+        app_version = config('HEROKU_RELEASE_VERSION', default=None)
+
+        if app_version:
+            mail_admins(
+                f'Aplicação iniciada {app_version}',
+                f"A aplicação foi iniciada com sucesso, versao {app_version}",
+                fail_silentyly=False,
+            )

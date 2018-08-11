8a83df628e18cc1cf4e5f1cffe853d5a99310706
Lucas Polo <pradopolo@Wgmail.com>
2018-08-11 09:57:36 -0300

    Criada view e formulário de vendas.
    close #94

--- vendas/urls.py
+++ vendas/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from vendas.views import DashboardView
+from vendas.views import DashboardView, NovoPedido
 
 urlpatterns = [
-    path('dashboard', DashboardView.as_view(), name="dashboard"),
+    path('dashboard', DashboardView.as_view(), name="novo-pedido"),
+    path('novo-pedido', NovoPedido.as_view(), name="dashboard"),
 ]

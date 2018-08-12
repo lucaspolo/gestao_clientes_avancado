7e3ab3cd7b3f0e551d4541a1acf3e97449d89b63
Lucas Polo <pradopolo@Wgmail.com>
2018-08-12 20:45:26 -0300

    Refatorado para poder editar venda
    close #102

--- vendas/urls.py
+++ vendas/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from vendas.views import DashboardView, NovoPedido, NovoItemPedido, ListaVendas
+from vendas.views import DashboardView, NovoPedido, NovoItemPedido, ListaVendas, EditPedido
 
 urlpatterns = [
     path('', ListaVendas.as_view(), name="lista-vendas"),
     path('dashboard', DashboardView.as_view(), name="dashboard"),
     path('novo-pedido', NovoPedido.as_view(), name="novo-pedido"),
+    path('edit-pedido/<int:venda>', EditPedido.as_view(), name="edit-item-pedido"),
     path('novo-item-pedido/<int:venda>', NovoItemPedido.as_view(), name="novo-item-pedido"),
 ]

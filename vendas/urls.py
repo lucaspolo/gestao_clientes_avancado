61f8f1890150ea688af2328fb521959c5137aeb4
Lucas Polo <pradopolo@Wgmail.com>
2018-08-13 13:49:06 -0300

    Criado link para exclusão de itens de vendas, confirmando.
    close #106

--- vendas/urls.py
+++ vendas/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from vendas.views import DashboardView, NovoPedido, NovoItemPedido, ListaVendas, EditPedido, DeletePedido
+from vendas.views import DashboardView, NovoPedido, NovoItemPedido, ListaVendas, EditPedido, DeletePedido, \
+    DeleteItemPedido
 
 urlpatterns = [
     path('', ListaVendas.as_view(), name="lista-vendas"),
     path('dashboard', DashboardView.as_view(), name="dashboard"),
     path('novo-pedido', NovoPedido.as_view(), name="novo-pedido"),
-    path('edit-pedido/<int:venda>', EditPedido.as_view(), name="edit-item-pedido"),
+    path('edit-pedido/<int:venda>', EditPedido.as_view(), name="edit-pedido"),
     path('delete-pedido/<int:venda>', DeletePedido.as_view(), name="delete-pedido"),
     path('novo-item-pedido/<int:venda>', NovoItemPedido.as_view(), name="novo-item-pedido"),
+    path('delete-item-pedido/<int:item>', DeleteItemPedido.as_view(), name="delete-item-pedido"),
 ]

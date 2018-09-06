ae8996c0a1df3f3fe6ef6526f4d1e5b145c72719
Lucas Polo <pradopolo@gmail.com>
2018-09-06 13:50:44 -0300

    Criada simples API para retornar produtos em JSON
    close #116

--- produtos/views.py
+++ produtos/views.py
@@ -1,3 +1,17 @@
+from django.forms import model_to_dict
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+from produtos.models import Produto
+
+
+def api(request):
+
+    produtos = Produto.objects.all()
+
+    lista_produtos = []
+    for produto in produtos:
+        lista_produtos.append(model_to_dict(produto))
+
+    return JsonResponse({'produtos': lista_produtos})

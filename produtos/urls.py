8967ae6abfed48e5e7a529ee08a11d77c8d3700e
Lucas Polo <pradopolo@gmail.com>
2018-09-06 13:50:44 -0300

    Criada simples API para retornar produtos em JSON
    close #116

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from produtos.views import api
+
 urlpatterns = [
     # path('', RedirectView.as_view(url=reverse_lazy('person_list')), name="root"),
     # path('list/', PersonList.as_view(), name="person_list"),
@@ -9,4 +11,5 @@ urlpatterns = [
     # path('update/<int:pk>/', PersonUpdate.as_view(), name="persons_update"),
     # path('delete/<int:pk>/', PersonDelete.as_view(), name="persons_delete"),
     # path('produto_bulk/', ProdutoBulk.as_view(), name="produto_delete"),
+    path('api/', api, name="api"),
 ]

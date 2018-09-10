d6ca0aab0ec8006008f0f678bc8a312891df4df3
Lucas Polo <pradopolo@gmail.com>
2018-09-10 12:54:17 -0300

    Criada API simples com CBV para recuperar dados de produto
    close #118

--- produtos/views.py
+++ produtos/views.py
@@ -3,6 +3,8 @@ from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.base import View
+
 from produtos.models import Produto
 
 
@@ -15,3 +17,9 @@ def api(request):
         lista_produtos.append(model_to_dict(produto))
 
     return JsonResponse({'produtos': lista_produtos})
+
+
+class APICBV(View):
+    def get(self, request, id):
+        produto = Produto.objects.get(id=id)
+        return JsonResponse(model_to_dict(produto))

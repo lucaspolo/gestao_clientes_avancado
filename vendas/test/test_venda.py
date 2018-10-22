636e956e7472afbd77c07b5c5e1a8bbab1ea60fa
Lucas Polo <pradopolo@gmail.com>
2018-10-22 14:16:50 -0300

    Criado teste para validar status de NFe e Venda
    close #134

--- vendas/test/test_venda.py
+++ vendas/test/test_venda.py
@@ -6,16 +6,28 @@ from vendas.models import Venda, ItemDoPedido
 
 class VendaTestCase(TestCase):
     def setUp(self):
-        self.venda = Venda.objects.create(numero="1234", desconto=10)
+        self.venda = Venda.objects.create(numero="1234", desconto=10, status=Venda.ABERTA)
         self.produto = Produto.objects.create(descricao="Produto 1", preco=10)
+        self.item = ItemDoPedido.objects.create(venda=self.venda, produto=self.produto, quantidade=10, desconto=10)
 
     def test_verifica_num_vendas(self):
         assert Venda.objects.all().count() == 1
 
     def test_valor_venda(self):
         """Verifica valor total da venda"""
-        ItemDoPedido.objects.create(venda=self.venda, produto=self.produto, quantidade=10, desconto=10)
+
         assert self.venda.valor == 80
 
     def test_desconto(self):
         assert self.venda.desconto == 10
+
+    def test_item_inlcuido_lista_itens(self):
+        self.assertIn(self.item, self.venda.itemdopedido_set.all())
+
+    def test_checa_nfe_nao_emitida(self):
+        self.assertFalse(self.venda.nfe_emitida)
+
+    def test_checa_status(self):
+        self.venda.status = Venda.PROCESSANDO
+        self.venda.save()
+        self.assertEqual(self.venda.status, Venda.PROCESSANDO)
